19ebf35f8e2b981461a2bc4903fa4ca4f015585f
Ross Jennings <rossjjennings@gmail.com>
2022-12-11 00:04:26 -0500

    Extract FigWrapper into a separate file

---
+++ figure.py
@@ -0,0 +1,38 @@
+from quart import websocket
+from matplotlib.backends.backend_webagg import new_figure_manager_given_figure
+import json
+
+class FigWrapper:
+    def __init__(self, tg, fig_id, fig):
+        self.tg = tg
+        self.fig = fig
+        self.manager = new_figure_manager_given_figure(fig_id, self.fig)
+        self.supports_binary = True
+        self.manager.add_web_socket(self)
+        print(f"Toolbar is: {self.manager.toolbar}")
+        tg.create_task(self.receive_messages())
+
+    def send_message(self, message):
+        self.tg.create_task(websocket.send(message))
+        
+    async def receive_messages(self):
+        while True:
+            message = await websocket.receive_json()
+            if message['type'] == 'supports_binary':
+                self.supports_binary = message['value']
+            else:
+                print(f"Received {message}")
+                self.manager.handle_json(message)
+
+    def send_json(self, content):
+        print(f"Sending JSON {content}")
+        self.send_message(json.dumps(content))
+
+    def send_binary(self, blob):
+        print(f"Sending blob")
+        if self.supports_binary:
+            self.send_message(blob)
+        else:
+            data_uri = "data:image/png;base64,{0}".format(
+                blob.encode('base64').replace('\n', ''))
+            self.write_message(data_uri)
--- server.py
+++ server.py
@@ -10,50 +10,13 @@ from quart import (
 import numpy as np
 import matplotlib as mpl
 from matplotlib.figure import Figure
-from matplotlib.backends.backend_webagg import (
-    FigureManagerWebAgg,
-    new_figure_manager_given_figure,
-)
+from matplotlib.backends.backend_webagg import FigureManagerWebAgg
 import os.path
 import asyncio
-import json
+
+from figure import FigWrapper
 
 app = Quart(__name__)
-
-class FigWrapper:
-    def __init__(self, tg, fig_id, fig):
-        self.tg = tg
-        self.fig = fig
-        self.manager = new_figure_manager_given_figure(fig_id, self.fig)
-        self.supports_binary = True
-        self.manager.add_web_socket(self)
-        print(f"Toolbar is: {self.manager.toolbar}")
-        tg.create_task(self.receive_messages())
-
-    def send_message(self, message):
-        self.tg.create_task(websocket.send(message))
-        
-    async def receive_messages(self):
-        while True:
-            message = await websocket.receive_json()
-            if message['type'] == 'supports_binary':
-                self.supports_binary = message['value']
-            else:
-                print(f"Received {message}")
-                self.manager.handle_json(message)
-
-    def send_json(self, content):
-        print(f"Sending JSON {content}")
-        self.send_message(json.dumps(content))
-
-    def send_binary(self, blob):
-        print(f"Sending blob")
-        if self.supports_binary:
-            self.send_message(blob)
-        else:
-            data_uri = "data:image/png;base64,{0}".format(
-                blob.encode('base64').replace('\n', ''))
-            self.write_message(data_uri)
 
 @app.route('/')
 async def hello_world():
