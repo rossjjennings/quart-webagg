0b90967dfb6a7ef55097837fe20b6e642e52e4de
Ross Jennings <rossjjennings@gmail.com>
2022-12-11 00:51:45 -0500

    Remove debug print statements

--- figure.py
+++ figure.py
@@ -9,7 +9,6 @@ class FigWrapper:
         self.manager = new_figure_manager_given_figure(fig_id, self.fig)
         self.supports_binary = True
         self.manager.add_web_socket(self)
-        print(f"Toolbar is: {self.manager.toolbar}")
         tg.create_task(self.receive_messages())
 
     async def receive_messages(self):
@@ -18,15 +17,12 @@ class FigWrapper:
             if message['type'] == 'supports_binary':
                 self.supports_binary = message['value']
             else:
-                print(f"Received {message}")
                 self.manager.handle_json(message)
 
     def send_json(self, content):
-        print(f"Sending JSON {content}")
         self.tg.create_task(websocket.send_json(content))
 
     def send_binary(self, blob):
-        print(f"Sending blob")
         if self.supports_binary:
             self.tg.create_task(websocket.send(blob))
         else:
