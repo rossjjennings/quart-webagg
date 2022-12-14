cc45cc06a8abb995300e3f7d49f11d93bf9ab620
Ross Jennings <rossjjennings@gmail.com>
2022-12-13 19:52:41 -0500

    Don't use name as figure ID

--- templates/mpl_figure.js
+++ templates/mpl_figure.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   var websocket_type = mpl.get_websocket_type();
   {% for fig in figures %}
   var {{ fig.name }} = new mpl.figure(
-      {{ fig.name }},
+      {{ fig.fig_id }},
       new websocket_type("{{ fig.sock_uri }}"),
       (figure, format) => {
         window.open("{{ fig.name }}." + format, "_blank");
