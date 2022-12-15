03f088eb81925a46280de91cf37bba6dbf2e3cad
Ross Jennings <rossjjennings@gmail.com>
2022-12-14 21:49:27 -0500

    Use descriptive names for figures

--- server.py
+++ server.py
@@ -12,19 +12,19 @@ sc = Sculptor(app)
 async def index():
     return await render_template('index.html')
 
-@sc.figure('figure1')
-def make_figure(fig):
+@sc.figure('sinusoid')
+def plot_sinusoid(fig):
     ax = fig.add_subplot()
     t = np.arange(0.0, 3.0, 0.01)
     s = np.sin(2 * np.pi * t)
     ax.plot(t, s)
     return fig
 
-@sc.figure('figure2')
-def make_figure(fig):
+@sc.figure('parabola')
+def plot_parabola(fig):
     ax = fig.add_subplot()
     t = np.arange(-1.0, 1.0, 0.01)
-    s = np.sin(t**2)
+    s = t**2
     ax.plot(t, s)
     return fig
 
